d59ec71a8828d4ef0964d1e592cc960d71626b2c
Naji <lemiine987@gmail.com>
2023-03-28 13:47:26 +0300

    Authentication Level 1 (very weak security)

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Require packages
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
-const exp = require('constants');
 
 // Create the app
 const app = express();
@@ -15,6 +15,69 @@ app.use(express.static("public"));
 // Set up body-parser
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+// Connect to the MongoDB database
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+// Create a schema for articles
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.route("/login")
+
+.get(function(req, res){
+    res.render("login");
+})
+
+.post(async function(req, res){
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+          // User not found in the database
+          return res.render("login", { error: "Invalid email or password" });
+        }
+        const passwordMatch = await User.findOne({password: req.body.password});
+        if (!passwordMatch) {
+          // Password does not match
+          return res.render("login", { error: "Invalid email or password" });
+        }
+        // Email and password match, set session and redirect to secrets
+        // req.session.user = user;
+        res.render("secrets");
+      } catch (err) {
+        console.error(err);
+        res.render("error", { error: "An error occurred" });
+      }
+});
+
+
+app.post("/register", async function(req, res) {
+    try {
+      const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+      });
+      const savedUser = await newUser.save();
+      if (savedUser) {
+        res.render("secrets");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+});
 
 
 
